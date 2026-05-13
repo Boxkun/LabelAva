03eea107a9ed2896b8c47353af5b229e2b421a3c
Boxkun <50022810+Boxkun@users.noreply.github.com>
2026-05-14 00:59:42 +0800

    misc: configure github actions misc: dynamic commit hash in about

--- Views/AboutWindow.axaml.cs
+++ Views/AboutWindow.axaml.cs
@@ -4,11 +4,15 @@ using Avalonia.Interactivity;
 
 namespace LabelAva.Views;
 
+
+
 public partial class AboutWindow : Window
 {
     public AboutWindow()
     {
         InitializeComponent();
+        VersionText.Text = $"Commit {BuildInfo.CommitHash}";
+        BuildTimeText.Text = $"构建日期 {BuildInfo.BuildTime}";
     }
 
     private void OnClose(object? sender, RoutedEventArgs e)
